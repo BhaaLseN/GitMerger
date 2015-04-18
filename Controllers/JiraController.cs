c4250611a29799878aacc5f42ca2e486d5d597c7
BhaaL <bhaal@0x1337.org>
2015-04-18 22:46:12 +0200

    add dummy controller for jira webhooks

---
+++ Controllers/JiraController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Web.Http;
+
+namespace GitMerger.Controllers
+{
+    public class JiraController : ApiController
+    {
+        public string Get()
+        {
+            return "nuuuuh";
+        }
+        public void Post(string json)
+        {
+        }
+    }
+}
