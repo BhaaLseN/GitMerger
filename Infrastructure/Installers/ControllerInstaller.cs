cb7aaa05b48fe92951607779722ce1ad89339b35
BhaaL <bhaal@0x1337.org>
2015-04-19 16:50:52 +0200

    install all controllers, since WebAPI will ask for them
    this isn't necessary for controllers that have default constructors, but
    all other controllers (that want DI) would fail otherwise.

---
+++ Infrastructure/Installers/ControllerInstaller.cs
@@ -0,0 +1,30 @@
+using System.Web.Http;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace GitMerger.Infrastructure.Installers
+{
+    /// <summary>
+    /// Registers all controllers for use with the dependency resolver.
+    /// </summary>
+    /// <remarks>
+    /// Apparently, the dependency resolver does not do any IoC on its own and therefore fails on controllers
+    /// that do not have a default constructor. Windsor needs to take care of those, so just register them.
+    /// WebAPI will call <see cref="IDependencyScope.GetService"/> for pretty much everything, including the controllers.
+    /// </remarks>
+    public class ControllerInstaller : IWindsorInstaller
+    {
+        #region IWindsorInstaller Members
+
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                .BasedOn<ApiController>()
+                // scope is based on IDependencyResolver.BeginScope, which seems to be per request
+                .LifestyleScoped());
+        }
+
+        #endregion
+    }
+}
