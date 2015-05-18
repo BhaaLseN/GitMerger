861260f16f6b9e24f7f407c2e9e9f6a7b273bc02
BhaaL <bhaal@0x1337.org>
2015-05-18 20:10:18 +0200

    fix ReadAsStringAsync throwing an ObjectDisposedException after the 2nd request
    simple fix: use "async Task" instead of "async void"
    explaination on why is not so simple; see http://stackoverflow.com/a/8043882

--- Controllers/JiraController.cs
+++ Controllers/JiraController.cs
@@ -1,10 +1,7 @@
 using System.Linq;
 using System.Net.Http;
-using System.Runtime.Serialization.Json;
-using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
-using System.Xml;
-using System.Xml.Linq;
 using GitMerger.Git;
 using GitMerger.Infrastructure.Settings;
 using GitMerger.Jira;
@@ -27,7 +24,12 @@ namespace GitMerger.Controllers
             return "nuuuuh";
         }
         [HttpPost]
-        public async void Post(HttpRequestMessage request)
+        // must return a Task rather than void, or request.Content.ReadAsStringAsync will throw an ObjectDisposedException
+        // after the first request was successful. Apparently ASP.Net (and also WebApi) do not like "fire and forget" methods.
+        // Eric Lippert explains this in http://stackoverflow.com/a/8043882 (basically: nothing can "await" an "async void"
+        // method, but that in turn doesn't allow the framework to know when to properly free/dispose/recycle/whatever the stuff
+        // passed in. in short: use "async Task" even if you won't return anything :S)
+        public async Task Post(HttpRequestMessage request)
         {
             Post(await request.Content.ReadAsStringAsync());
         }
