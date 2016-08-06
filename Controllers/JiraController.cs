28aafc9500fb7adbc6f9427110b6ed960708f7ba
BhaaL <bhaal@0x1337.org>
2016-08-06 15:10:52 +0200

    move the issue detail update to GitMerger too
    we might need to call it multiple times to make sure we're up-to-date

--- Controllers/JiraController.cs
+++ Controllers/JiraController.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -43,50 +42,6 @@ namespace GitMerger.Controllers
             string transitionUserName = issueDetails.TransitionUserName;
             string transitionUserMail = issueDetails.TransitionUserEMail;
             var mergeRequest = new MergeRequest(transitionUserName, transitionUserMail, issueDetails);
-
-            if (issueDetails.CustomFields != null)
-            {
-                // see if we have an upstream branch name set; so we can override the initial guess of "master"
-                if (!string.IsNullOrEmpty(_jiraSettings.UpstreamBranchFieldName) &&
-                    issueDetails.CustomFields.Contains(_jiraSettings.UpstreamBranchFieldName))
-                {
-                    Logger.Debug(m => m("Checking {0} potential upstream branches: {1}",
-                        issueDetails.CustomFields[_jiraSettings.UpstreamBranchFieldName].Count(),
-                        string.Join(", ", issueDetails.CustomFields[_jiraSettings.UpstreamBranchFieldName])));
-                    // technically, this could be a list or something. we'll just ignore all but the first one and hope it works out.
-                    string upstreamBranch = issueDetails.CustomFields[_jiraSettings.UpstreamBranchFieldName].FirstOrDefault();
-                    if (!string.IsNullOrWhiteSpace(upstreamBranch))
-                    {
-                        Logger.Info(m => m("Merge request for '{0}' uses upstream branch '{1}' instead of default '{2}'.", mergeRequest.BranchName, upstreamBranch, mergeRequest.UpstreamBranch));
-                        mergeRequest.UpstreamBranch = upstreamBranch;
-                    }
-                    else
-                    {
-                        Logger.Info(m => m("Merge request for '{0}' uses default upstream branch '{1}'.", mergeRequest.BranchName, mergeRequest.UpstreamBranch));
-                    }
-                }
-
-                // see if we have an actual branch name set instead of using the issue key
-                if (!string.IsNullOrEmpty(_jiraSettings.BranchFieldName) &&
-                    issueDetails.CustomFields.Contains(_jiraSettings.BranchFieldName))
-                {
-                    Logger.Debug(m => m("Checking {0} potential branch names: {1}",
-                        issueDetails.CustomFields[_jiraSettings.BranchFieldName].Count(),
-                        string.Join(", ", issueDetails.CustomFields[_jiraSettings.BranchFieldName])));
-                    // technically, this could be a list or something. we'll just ignore all but the first one and hope it works out.
-                    string branchName = issueDetails.CustomFields[_jiraSettings.BranchFieldName].FirstOrDefault();
-                    if (!string.IsNullOrWhiteSpace(branchName))
-                    {
-                        Logger.Info(m => m("Merge request for '{0}' uses branch name '{1}' instead of issue key.", mergeRequest.BranchName, branchName));
-                        mergeRequest.BranchName = branchName;
-                        mergeRequest.BranchNameIsExact = true;
-                    }
-                    else
-                    {
-                        Logger.Info(m => m("Merge request for '{0}' uses issue key as branch name.", mergeRequest.BranchName));
-                    }
-                }
-            }
 
             TriggerMerge(mergeRequest);
         }
--- Git/GitMerger.cs
+++ Git/GitMerger.cs
@@ -31,6 +31,9 @@ namespace GitMerger.Git
 
         public void QueueRequest(MergeRequest mergeRequest)
         {
+            // initially update the merge request with information from the POST request (or wherever it came from).
+            // we might do another update later to make sure the data is still relevant.
+            UpdateIssueDetails(mergeRequest, mergeRequest.IssueDetails);
             if (!ShouldTryToMerge(mergeRequest))
                 return;
 
@@ -70,6 +73,55 @@ namespace GitMerger.Git
         }
 
         #endregion
+
+        private void UpdateIssueDetails(MergeRequest mergeRequest, IssueDetails issueDetails)
+        {
+            if (mergeRequest == null || issueDetails == null)
+                return;
+            if (issueDetails.CustomFields != null)
+            {
+                // see if we have an upstream branch name set; so we can override the initial guess of "master"
+                if (!string.IsNullOrEmpty(_jiraSettings.UpstreamBranchFieldName) &&
+                    issueDetails.CustomFields.Contains(_jiraSettings.UpstreamBranchFieldName))
+                {
+                    Logger.Debug(m => m("Checking {0} potential upstream branches: {1}",
+                        issueDetails.CustomFields[_jiraSettings.UpstreamBranchFieldName].Count(),
+                        string.Join(", ", issueDetails.CustomFields[_jiraSettings.UpstreamBranchFieldName])));
+                    // technically, this could be a list or something. we'll just ignore all but the first one and hope it works out.
+                    string upstreamBranch = issueDetails.CustomFields[_jiraSettings.UpstreamBranchFieldName].FirstOrDefault();
+                    if (!string.IsNullOrWhiteSpace(upstreamBranch))
+                    {
+                        Logger.Info(m => m("Merge request for '{0}' uses upstream branch '{1}' instead of default '{2}'.", mergeRequest.BranchName, upstreamBranch, mergeRequest.UpstreamBranch));
+                        mergeRequest.UpstreamBranch = upstreamBranch;
+                    }
+                    else
+                    {
+                        Logger.Info(m => m("Merge request for '{0}' uses default upstream branch '{1}'.", mergeRequest.BranchName, mergeRequest.UpstreamBranch));
+                    }
+                }
+
+                // see if we have an actual branch name set instead of using the issue key
+                if (!string.IsNullOrEmpty(_jiraSettings.BranchFieldName) &&
+                    issueDetails.CustomFields.Contains(_jiraSettings.BranchFieldName))
+                {
+                    Logger.Debug(m => m("Checking {0} potential branch names: {1}",
+                        issueDetails.CustomFields[_jiraSettings.BranchFieldName].Count(),
+                        string.Join(", ", issueDetails.CustomFields[_jiraSettings.BranchFieldName])));
+                    // technically, this could be a list or something. we'll just ignore all but the first one and hope it works out.
+                    string branchName = issueDetails.CustomFields[_jiraSettings.BranchFieldName].FirstOrDefault();
+                    if (!string.IsNullOrWhiteSpace(branchName))
+                    {
+                        Logger.Info(m => m("Merge request for '{0}' uses branch name '{1}' instead of issue key.", mergeRequest.BranchName, branchName));
+                        mergeRequest.BranchName = branchName;
+                        mergeRequest.BranchNameIsExact = true;
+                    }
+                    else
+                    {
+                        Logger.Info(m => m("Merge request for '{0}' uses issue key as branch name.", mergeRequest.BranchName));
+                    }
+                }
+            }
+        }
 
         private bool ShouldTryToMerge(MergeRequest mergeRequest)
         {
