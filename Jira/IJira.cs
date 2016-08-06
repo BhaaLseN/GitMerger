18fcff94032b432ee7e6aee3ed8247b7eaf8fe9f
BhaaL <bhaal@0x1337.org>
2016-08-06 15:03:38 +0200

    remove the now unused Jira.IsClosed method

--- Jira/IJira.cs
+++ Jira/IJira.cs
@@ -1,11 +1,8 @@
-using System;
-
 namespace GitMerger.Jira
 {
     public interface IJira
     {
         IssueDetails GetIssueDetails(string issueKey);
         void PostComment(string issueKey, string comment);
-        bool IsClosed(IssueDetails issueDetails);
     }
 }
